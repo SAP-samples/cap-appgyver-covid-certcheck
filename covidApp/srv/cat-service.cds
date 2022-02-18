0d4cbbd5294714f2b1171af76eac9a2948b1db03
Martin Frick <martin.frick@sap.com>
2022-02-18 15:40:50 +0100

    Move from Security Subaccount

--- covidApp/srv/cat-service.cds
+++ covidApp/srv/cat-service.cds
@@ -1,9 +1,20 @@
 using bookshop as bookshop from '../db/data-model';
+using pkce_svc as pkce from '../db/pkce-service-model';
 
-service AuthenticatedService @(requires:'authenticated-user') {
-    @readonly entity Books as projection on bookshop.Books;
+service AuthenticatedService @(requires : 'authenticated-user') {
+    @readonly
+    entity Books as projection on bookshop.Books;
 }
 
-service AdminService @(requires:'admin') {
-    @readonly entity Authors as projection on bookshop.Authors;
+service AdminService @(requires : 'admin') {
+    @readonly
+    entity Authors as projection on bookshop.Authors;
+}
+
+service PkceService @(_requires : 'authenticated-user') {
+    view GenPkce as
+        select from pkce.pkce_code {
+            pkce_code.code_challenge as code_challenge,
+            pkce_code.code_verifier  as code_verifier
+        };
 }
--- covidApp/srv/cat-service.js
+++ covidApp/srv/cat-service.js
@@ -1,12 +1,29 @@
+const cds = require("@sap/cds");
+const crypto = require("crypto");
 
-const cds = require('@sap/cds')
+module.exports = cds.service.impl(function () {
+  this.on(["READ"], "Books", async (req) => {
+    if (req.user.is("admin")) {
+      return await SELECT.one("Books").where({ ID: 1 });
+    } else {
+      return await SELECT.one("Books").where({ ID: 2 });
+    }
+  });
+  this.on(["READ"], "GenPkce", () => {
+    let verifier = base64URLEncode(crypto.randomBytes(32));
+    let challenge = base64URLEncode(sha256(verifier));
+    return { code_challenge: challenge, code_verifier: verifier };
+  });
+});
 
-module.exports = cds.service.impl(function(){ 
-    this.on(['READ'], 'Books', async (req)=>{
-        if (req.user.is('admin')){
-            return await SELECT.one('Books').where({ ID: 1})
-        }else{
-            return await SELECT.one('Books').where({ ID: 2})
-        }
-    })
- })
+function base64URLEncode(str) {
+  return str
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+}
+
+function sha256(buffer) {
+  return crypto.createHash("sha256").update(buffer).digest();
+}
